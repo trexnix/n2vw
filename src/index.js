2c769b8a9f709ec0a0883f91c912eae269c38b5c
Trexnix <trexnix@gmail.com>
2017-08-31 02:37:00 +0700

    Make it work basically

--- src/index.js
+++ src/index.js
@@ -1,7 +1,120 @@
-class NumberToWords {
-  constructor() {
+const numberFormatValidator = /^\d+$/;
+const largeUnitsBase = ['nghìn', 'triệu', 'tỉ'];
+const basicNumberToWord = {
+  '0': 'không',
+  '1': 'một',
+  '2': 'hai',
+  '3': 'ba',
+  '4': 'bốn',
+  '5': 'năm',
+  '6': 'sáu',
+  '7': 'bảy',
+  '8': 'tám',
+  '9': 'chín'
+};
+const HUNDRED_PLACE_INDEX = 0;
+const TENS_PLACE_INDEX = 1;
+const ONES_PLACE_INDEX = 2;
 
+export default class NumberToWord {
+  constructor() {
+  }
+
+  static breakIntoGroupOfThree(string) {
+    let groups = [];
+    const remainder = string.length % 3;
+
+    if (remainder !== 0) {
+      groups.push(string.substr(0, remainder));
+      string = string.substr(remainder);
+    }
+    return groups.concat(string.match(/\d{1,3}/g));
+  }
+
+  static mapGroupsToUnits(groups) {
+    const reversedGroups = groups.slice().reverse();
+    let reversedUnits;
+
+    reversedUnits = reversedGroups.map((group, index) => {
+      if (index === 0) {
+        return '';
+      }
+      return largeUnitsBase[(index - 1) % 3];
+    });
+    return reversedUnits.reverse();
+  }
+
+  static translateThreeDigitsNumberToWords(number) {
+    number = String(number);
+
+    if (number.length > 3 || number.length < 0 || !numberFormatValidator.test(number)) {
+      throw TypeError;
+    }
+    const digitsLength = number.length;
+
+    return number.split('').map((digit, numberIndex) => {
+      let placeIndex = numberIndex;
+
+      if (digitsLength < 3) {
+        placeIndex = placeIndex + (3 - digitsLength);
+      }
+      switch (placeIndex) {
+        case HUNDRED_PLACE_INDEX: {
+          return basicNumberToWord[digit] + ' trăm';
+        }
+        case TENS_PLACE_INDEX: {
+          if (digit === '0') {
+            let nextDigest = number[numberIndex + 1];
+
+            if (nextDigest === '0') {
+              return '';
+            }
+
+            return 'lẻ';
+          }
+          if (digit === '1') {
+            return 'mười';
+          }
+          return basicNumberToWord[digit] + ' mươi';
+        }
+        case ONES_PLACE_INDEX: {
+          if (digit === '5' && digitsLength > 1) {
+            return 'lăm';
+          }
+          if (digit === '1' && digitsLength > 1) {
+            let nextDigest = number[numberIndex - 1];
+
+            if (nextDigest !== '0') {
+              return 'mốt';
+            }
+          }
+          if (digit === '0' && digitsLength > 1) {
+            return '';
+          }
+          return basicNumberToWord[digit];
+        }
+      }
+      return null;
+    }).filter(n => n !== '').join(' ');
+  }
+
+  static mapGroupsToWords(groups) {
+    return groups.map((number, index) => {
+      return this.translateThreeDigitsNumberToWords(number, index === 0);
+    });
+  }
+
+  getFullText(numberOrString) {
+    let number = String(numberOrString);
+    let groups = NumberToWord.breakIntoGroupOfThree(number);
+    let groupsToUnits = NumberToWord.mapGroupsToUnits(groups);
+    let groupsToWords = NumberToWord.mapGroupsToWords(groups);
+
+    return groupsToWords.map((word, index) => {
+      if (groupsToUnits[index] === '') {
+        return word;
+      }
+      return word + ' ' + groupsToUnits[index];
+    }).join(', ');
   }
 }
-
-export default NumberToWords;
--- test/n2vw.spec.js
+++ test/n2vw.spec.js
@@ -1,14 +1,51 @@
 /* global describe, it, before */
 
 import chai from 'chai';
-import n2vw from '../lib/n2vw.js';
+import Converter from '../lib/n2vw.js';
 
 chai.expect();
 
 const expect = chai.expect;
 
 describe('Given an instance of n2vw library', () => {
-  it('should return the name', () => {
-    expect(n2vw).to.be.exist;
+  describe('.breakIntoGroupOfThree', () => {
+    it('should break the number string into groups of three number', () => {
+      expect(Converter.breakIntoGroupOfThree('1234567')).to.be.eql(['1', '234', '567']);
+    });
+  });
+
+  describe('.mapGroupsToUnits', () => {
+    it('should groups of three number to respective units based on their places', () => {
+      expect(Converter.mapGroupsToUnits(['123'])).to.be.eql(['']);
+      expect(Converter.mapGroupsToUnits(['1', '234', '567'])).to.be.eql(['triệu', 'nghìn', '']);
+    });
+  });
+
+  describe('.translateThreeDigitsNumberToWords', () => {
+    it('should throw error if argument is invalid', () => {
+      expect(() => {
+        Converter.translateThreeDigitsNumberToWords(1234);
+      }).to.throw();
+    });
+
+    it('should translate it properly', () => {
+      expect(Converter.translateThreeDigitsNumberToWords(5)).to.be.eql('năm');
+      expect(Converter.translateThreeDigitsNumberToWords(10)).to.be.eql('mười');
+      expect(Converter.translateThreeDigitsNumberToWords(12)).to.be.eql('mười hai');
+      expect(Converter.translateThreeDigitsNumberToWords(15)).to.be.eql('mười lăm');
+      expect(Converter.translateThreeDigitsNumberToWords(20)).to.be.eql('hai mươi');
+      expect(Converter.translateThreeDigitsNumberToWords(99)).to.be.eql('chín mươi chín');
+      expect(Converter.translateThreeDigitsNumberToWords(100)).to.be.eql('một trăm');
+      expect(Converter.translateThreeDigitsNumberToWords(101)).to.be.eql('một trăm lẻ một');
+      expect(Converter.translateThreeDigitsNumberToWords(999)).to.be.eql('chín trăm chín mươi chín');
+    })
+  });
+
+  describe('#getFullText', () => {
+    it('should convert a number to words', () => {
+      let converter = new Converter;
+      expect(converter.getFullText(123456)).to.be.eql('một trăm hai mươi ba nghìn, bốn trăm năm mươi sáu');
+      expect(converter.getFullText(9987654321)).to.be.eql('chín tỉ, chín trăm tám mươi bảy triệu, sáu trăm năm mươi bốn nghìn, ba trăm hai mươi mốt');
+    });
   });
 });
